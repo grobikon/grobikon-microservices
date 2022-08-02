879d303f80961fdee611f83c2ef28459d5ec3aa9
Образцов Григорий <grobikon@mail.ru>
2022-08-02 10:44:49 +0300

    Added gateway.

---
+++ grobikon-gateway/src/main/java/ru/grobikon/grobikongateway/GrobikonGatewayApplication.java
@@ -0,0 +1,13 @@
+package ru.grobikon.grobikongateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrobikonGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrobikonGatewayApplication.class, args);
+    }
+
+}
