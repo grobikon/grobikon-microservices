99425c8d59a6a0517f14789c2c591a6d03ca7ab8
Образцов Григорий <grobikon@mail.ru>
2022-08-02 10:45:39 +0300

    Added gateway.

--- grobikon-gateway/src/main/java/ru/grobikon/grobikongateway/GrobikonGatewayApplication.java
+++ grobikon-gateway/src/main/java/ru/grobikon/grobikongateway/GrobikonGatewayApplication.java
@@ -2,8 +2,10 @@ package ru.grobikon.grobikongateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GrobikonGatewayApplication {
 
     public static void main(String[] args) {
