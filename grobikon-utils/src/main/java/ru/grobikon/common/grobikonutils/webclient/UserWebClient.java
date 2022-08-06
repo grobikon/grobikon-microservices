f7f63839832b424b1d297f27eb1be3c1bf225d0c
Образцов Григорий <grobikon@mail.ru>
2022-08-06 17:53:07 +0300

    Added WebFlux WebClient.

---
+++ grobikon-utils/src/main/java/ru/grobikon/common/grobikonutils/webclient/UserWebClient.java
@@ -0,0 +1,34 @@
+package ru.grobikon.common.grobikonutils.webclient;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import ru.grobikon.common.grobikoncommonentity.entity.User;
+
+@Component
+public class UserWebClient {
+
+    private static final String BASE_URL = "http://localhost:8765/grobikon-users/user";
+
+    /**
+     * Проверка - существует ли пользователь
+     */
+    public boolean userExists(Long userId) {
+
+        try{
+            var user = WebClient.create(BASE_URL)
+                    .post()
+                    .uri("id")
+                    .bodyValue(userId)
+                    .retrieve()
+                    .bodyToFlux(User.class)
+                    .blockFirst(); //Блокируем поток до получения 1й записи
+
+            //вызов сервиса
+            return user != null;
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false; // если статус не был 200
+    }
+}
